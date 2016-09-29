7cb5648b1faa5b58b637592602252bddfdd8a229
NickVermeylen <nick.vermeylen@student.thomasmore.be>
2016-09-29 17:17:15 +0200

    Looplicht pi

---
+++ looplicht.c
@@ -0,0 +1,41 @@
+#include <bcm2835.h>
+#include <stdio.h>
+#include <stdint>
+
+#define AANTAL 8
+#define PIN1 RPI_GPIO_P1_11
+#define PIN2 RPI_GPIO_P1_12
+#define PIN3 RPI_GPIO_P1_13
+#define PIN4 RPI_GPIO_P1_15
+#define PIN5 RPI_GPIO_P1_16
+#define PIN6 RPI_GPIO_P1_18
+#define PIN7 RPI_GPIO_P1_8
+#define PIN8 RPI_GPIO_P1_7
+
+int main(int argc, char **argv)
+{
+	if(!bcm2835_init())
+	{
+		return 1;
+	}
+
+	uint8_t leds[AANTAL] = {PIN1,PIN2,PIN3,PIN4,PIN5,PIN6,PIN7,PIN8};
+
+	bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
+
+	while(1)
+	{
+		bcm2835_gpio_write(leds[0], HIGH);
+		for(int i = 1; i < 8; i++)
+		{
+		bcm2835_gpio_write(leds[i-1], LOW);
+		bcm2835_delay(500);
+		bcm2835_gpio_write(leds[i], HIGH);
+		bcm2835_delay(500);
+		}
+		bcm2835_gpio_write(leds[7], LOW);
+	}
+
+	bcm2835_close();
+	return 0;
+}
